84e19ba892a6bb78db8b53b11da35ffdea32d29b
Matt Layher <mdlayher@gmail.com>
2015-05-24 14:27:21 -0400

    dhcp6: clarify DHCP means DHCPv6

--- dhcp6.go
+++ dhcp6.go
@@ -1,4 +1,7 @@
 // Package dhcp6 implements a DHCPv6 server, as described in IETF RFC 3315.
+//
+// Unless otherwise stated, any reference to "DHCP" in this package refers to
+// DHCPv6 only.
 package dhcp6
 
 //go:generate stringer -output=string.go -type=DUIDType,MessageType,Status,OptionCode
