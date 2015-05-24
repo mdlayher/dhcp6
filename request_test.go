07951b24aea034e650ae7a8d878ca15c0e2e35e4
Matt Layher <mdlayher@gmail.com>
2015-05-24 14:13:28 -0400

    request_test: initial commit of test for newServerRequest

---
+++ request_test.go
@@ -0,0 +1,41 @@
+package dhcp6
+
+import (
+	"net"
+	"reflect"
+	"testing"
+)
+
+// Test_newServerRequest verifies that newServerRequest returns a consistent
+// Request struct for use in Handler types.
+func Test_newServerRequest(t *testing.T) {
+	option := Option{
+		Code: OptionClientID,
+		Data: []byte{0, 1},
+	}
+	p, err := newPacket(MessageTypeSolicit, []byte{1, 2, 3}, []Option{option})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	addr := &net.UDPAddr{
+		IP:   net.ParseIP("::1"),
+		Port: 546,
+	}
+
+	r := &Request{
+		MessageType:   p.MessageType(),
+		TransactionID: p.TransactionID(),
+		Options:       make(Options),
+		Length:        int64(len(p)),
+		RemoteAddr:    "[::1]:546",
+
+		packet: p,
+	}
+	r.Options.Add(option.Code, option.Data)
+
+	if want, got := r, newServerRequest(p, addr); !reflect.DeepEqual(want, got) {
+		t.Fatalf("unexpected Request for newServerRequest(%v, %v)\n- want: %v\n-  got: %v",
+			p, addr, want, got)
+	}
+}
