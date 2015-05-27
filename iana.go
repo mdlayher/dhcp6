46bf7f231460c2e2f569a0cb603048bfef8bdd83
Matt Layher <mdlayher@gmail.com>
2015-05-27 00:50:48 -0400

    iana: improve documentation for types

--- iana.go
+++ iana.go
@@ -17,10 +17,13 @@ var (
 )
 
 // IANA represents an Identity Association for Non-temporary Addresses, as
-// defined in IETF RFC 3315, Section 10.  DHCP clients and servers use
-// identity assocations (IAs) to identify, group, and manage a set of
-// related IPv6 addresses.  Each IA must be associated with exactly one
-// network interface.  Multiple IAs may be present in a DHCP request.
+// defined in IETF RFC 3315, Section 10.
+//
+// DHCP clients and servers use identity assocations (IAs) to identify, group,
+// and manage a set of related IPv6 addresses.  Each IA must be associated
+// with exactly one network interface.
+//
+// Multiple IAs may be present in a single DHCP request.
 type IANA struct {
 	// The raw byte slice containing the IANA, with options stripped
 	iana []byte
@@ -29,9 +32,12 @@ type IANA struct {
 	options Options
 }
 
-// NewIANA creates a new IANA from an IAID, T1 and T2 durations, and
-// an optional Options map.  The IAID must be exactly 4 bytes in length.
-// If an Options map is not specified, a new one will be allocated.
+// NewIANA creates a new IANA from an IAID, T1 and T2 durations, and an
+// optional Options map.
+//
+// The IAID must be exactly 4 bytes in length, and the same value must
+// always be produced across restarts of a client. If an Options map is not
+// specified, a new one will be allocated.
 func NewIANA(iaid []byte, t1 time.Duration, t2 time.Duration, options Options) (*IANA, error) {
 	// IANA is always 4 bytes
 	if len(iaid) != 4 {
@@ -56,9 +62,9 @@ func NewIANA(iaid []byte, t1 time.Duration, t2 time.Duration, options Options) (
 	}, nil
 }
 
-// Bytes returns the underlying byte slice for an IANA, as well as a
-// byte slice for all options which have been applied to the Options
-// map for this IANA.
+// Bytes implements Byteser, and returns the underlying byte slice for an IANA,
+// appended with a byte slice of all options which have been applied to the
+// Options map for this IANA.
 func (i *IANA) Bytes() []byte {
 	// Enumerate optslice and check byte count
 	opts := i.options.enumerate()
@@ -73,8 +79,7 @@ func (i *IANA) Bytes() []byte {
 }
 
 // IAID returns an identity association identifier, which is a value generated
-// by a client, chosen to be unique among other IAs for that client.  An IAID
-// must always produce the same value across restarts of a client.
+// by a client, chosen to be unique among other IAs for that client.
 func (i *IANA) IAID() []byte {
 	// Too short to contain IAID
 	if len(i.iana) < 4 {
