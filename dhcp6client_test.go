9626061f379dde6dd1d2d47d604426bc69999ff6
Ronald G. Minnich <rminnich@gmail.com>
2017-08-24 08:04:48 -0700

    dhcp6client: fix faulting error in test
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- dhcp6client_test.go
+++ dhcp6client_test.go
@@ -23,29 +23,38 @@ func TestSolicitAndAdvertise(t *testing.T) {
 
 	reply, err := serve(r)
 	if err != nil {
-		t.Fatalf("%v\n", err)
+		t.Fatalf("%v", err)
+	}
+	t.Logf("Got a reply: %v", reply)
+	if reply == nil {
+		t.Fatalf("The reply is nil?")
 	}
 
 	if reply.MessageType != MessageTypeAdvertise {
-		t.Fatalf("Reply does not have a correct type\nShould be MessageTypeAdvertise but %v instead\n", MessageType(reply.MessageType))
+		t.Fatalf("Reply does not have a correct typeShould be MessageTypeAdvertise but %v instead\n", MessageType(reply.MessageType))
 	}
+	t.Logf("It is a MessageTypeAdvertise")
 	if !reflect.DeepEqual(reply.TransactionID, [3]byte{0x00, 0x01, 0x02}) {
-		t.Fatalf("Reply txID does not match\n")
+		t.Fatalf("Reply txID does not match")
 	}
+	t.Logf("txID matches")
 	iana, containsIANA, err := reply.Options.IANA()
 	if !containsIANA {
-		t.Fatalf("Reply does not contain IANA\n")
+		t.Fatalf("Reply does not contain IANA")
 	}
+	t.Logf("It contains an IANA")
 	if err != nil {
-		t.Fatalf("Reply does not contain valid IANA\n")
+		t.Fatalf("Reply does not contain valid IANA")
 	}
 
+	t.Logf("The IANA is valid")
 	iaaddr, containsIAAddr, err := iana[0].Options.IAAddr()
 	if !containsIAAddr {
-		t.Fatalf("Reply does not contain IAAddr\n")
+		t.Fatalf("Reply does not contain IAAddr")
 	}
+	t.Logf("It contains an IAAddr")
 	if err != nil {
-		t.Fatalf("Reply does not contain valid IAAddr\n")
+		t.Fatalf("Reply does not contain valid IAAddr")
 	}
-	t.Logf("Get assigned ipv6 addr from server: %+v\n", iaaddr[0])
+	t.Logf("Get assigned ipv6 addr from server: %+v", iaaddr[0])
 }
